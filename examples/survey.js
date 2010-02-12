1ef186e896da2b4f5cecbbb54bfb498bce030971
Nick Fitzgerald <fitzgen@gmail.com>
2010-02-11 23:49:02 -0800

    Adding example and a little tidbit of info in the README

---
+++ examples/survey.js
@@ -0,0 +1,45 @@
+var JACK = require("jack");
+var REFORM = require("reform");
+
+var survey = new REFORM.Form({
+    first_name: new REFORM.TextField(),
+    last_name: new REFORM.TextField(),
+    age: new REFORM.IntegerField(),
+    email: new REFORM.EmailField({
+        required: false,
+        help_text: "I know some people are scared to get spam, so this field is optional."
+    }),
+    season: new REFORM.DropdownField({
+        help_text: "What season is it right now?",
+        choices: [
+            ["su", "Summer"],
+            ["w", "Winter"],
+            ["sp", "Spring"],
+            ["f", "Fall"]
+        ]
+    }),
+    random: new REFORM.TextAreaField({
+        label: "About me.",
+        help_text: "Tell the world about anything about yourself!"
+    }),
+    submit: new REFORM.Button()
+});
+
+exports.app = function (env) {
+    var resp = new JACK.Response();
+    var req = new JACK.Request(env);
+    var form = survey.clone(req.isPost() ? req.POST() : null);
+    
+    resp.setHeader("Content-Type", "text/html");
+    
+    if (req.isPost()) {
+        if (form.is_valid()) {
+            resp.write("<p>The form is valid!</p>");
+        } else {
+            resp.write("<p>The form is not valid!</p>");
+        }
+    }
+
+    resp.write(form.toHtml("div"));
+    return resp.finish();
+};
