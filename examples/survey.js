a014db3a19f86e657ca30ffea68efc24766a2480
Nick Fitzgerald <fitzgen@gmail.com>
2010-02-19 10:53:45 -0800

    updating the survey example to work with the newer js code style

--- examples/survey.js
+++ examples/survey.js
@@ -2,15 +2,15 @@ var JACK = require("jack");
 var REFORM = require("reform");
 
 var survey = new REFORM.Form({
-    first_name: new REFORM.TextField(),
-    last_name: new REFORM.TextField(),
+    firstName: new REFORM.TextField(),
+    lastName: new REFORM.TextField(),
     age: new REFORM.IntegerField(),
     email: new REFORM.EmailField({
         required: false,
-        help_text: "I know some people are scared to get spam, so this field is optional."
+        helpText: "I know some people are scared to get spam, so this field is optional."
     }),
     season: new REFORM.DropdownField({
-        help_text: "What season is it right now?",
+        helpText: "What season is it right now?",
         choices: [
             ["su", "Summer"],
             ["w", "Winter"],
@@ -20,7 +20,7 @@ var survey = new REFORM.Form({
     }),
     random: new REFORM.TextAreaField({
         label: "About me.",
-        help_text: "Tell the world about anything about yourself!"
+        helpText: "Tell the world about anything about yourself!"
     }),
     submit: new REFORM.Button()
 });
@@ -29,11 +29,11 @@ exports.app = function (env) {
     var resp = new JACK.Response();
     var req = new JACK.Request(env);
     var form = survey.clone(req.isPost() ? req.POST() : null);
-    
+
     resp.setHeader("Content-Type", "text/html");
-    
-    if (req.isPost()) {
-        if (form.is_valid()) {
+
+    if (form.isBound) {
+        if (form.isValid()) {
             resp.write("<p>The form is valid!</p>");
         } else {
             resp.write("<p>The form is not valid!</p>");
